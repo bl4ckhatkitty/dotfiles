f424a9fa6200508f2cc1cb30cae549847d722692
bl4ckhatkitty <96919761+bl4ckhatkitty@users.noreply.github.com>
2022-02-22 18:36:13 +0000

    update

---
+++ .config/fish/config.fish
@@ -0,0 +1,12 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set fish_greeting ""
+
+# aliases
+alias ll="exa -l"
+alias ls="exa -l"
+alias l="exa -a -l"
+alias tree="exa -a -l --icons --tree"
+alias rr="ranger"
